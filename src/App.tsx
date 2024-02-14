804d123010c3a13410fe43d86562edd15592aea0
Khyati Bardolia <khyati.bardolia@gmail.com>
2024-02-14 15:33:29 -0500

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {DateRangePicker} from "./components/DateRangePicker/DateRangePicker";
+
+function App() {
+
+  return (
+     <DateRangePicker/>
+  )
+}
+
+export default App
---
+++ src/components/DateRangePicker/DateRangePicker.tsx
@@ -0,0 +1,5 @@
+import {FC} from "react";
+
+export const DateRangePicker: FC = () => {
+    return <>DateRangePicker component</>
+}
